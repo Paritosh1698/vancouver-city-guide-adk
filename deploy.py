b0bd1dcc372004ae84a3d1b04505903749d95164
Paritosh Shelke <66405477+Paritosh1698@users.noreply.github.com>
2026-01-13 17:40:44 -0500

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -2,14 +2,12 @@ import vertexai
 from vertexai import agent_engines
 from vancouver_city_guide.agent import root_agent
 
-# CONFIGURATION
 PROJECT_ID = "adk-agent-481717"
 REGION = "us-central1"
 STAGING_BUCKET = "gs://adk-agent-481717-adk-staging"
 
 vertexai.init(project=PROJECT_ID, location=REGION, staging_bucket=STAGING_BUCKET)
 
-# 1. Define the requirements exactly
 requirements = [
     "google-adk>=1.15.1",
     "google-genai",
@@ -18,8 +16,6 @@ requirements = [
     "python-dotenv",
     ]
 
-# 2. Package your local code
-# This tells Vertex AI to include these folders in the remote container's PYTHONPATH
 extra_packages = [
     "./vancouver_city_guide",  # Your main folder
 ]
@@ -27,7 +23,6 @@ extra_packages = [
 print("Starting deployment to Agent Engine...")
 
 try:
-    # We wrap the ADK agent in an AdkApp for the Reasoning Engine
     from vertexai.preview import reasoning_engines
 
     adk_app = reasoning_engines.AdkApp(
@@ -35,7 +30,6 @@ try:
         enable_tracing=True,
     )
 
-    # Create the remote resource
     remote_app = agent_engines.create(
         agent_engine=adk_app,
         requirements=requirements,
